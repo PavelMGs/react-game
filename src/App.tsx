e3a143acf5597b626f02e135a2da016d6b1f1ac9
PavelMGs <pavelmg99@gmail.com>
2021-02-20 20:41:02 +0200

    init: start react-game task

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            Here will be a Snake   
+        </div>
+    )
+}
+
+export default App;
+
+//webpack --config webpack.config.js
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const NODE_ENV = process.env.NODE_ENV;
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    resolve: {
+        extensions: ['.ts', '.jsx', '.js', '.json', '.tsx'],
+    },
+    mode: NODE_ENV ? NODE_ENV : 'development',
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    watchOptions: {
+        ignored: /node_modules/,
+        poll: 1000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.[tj]sx?$/,
+                exclude: [/node_modules/],
+                use: ['ts-loader'],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-modules-typescript-loader?modules',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                mode: 'local',
+                                localIdentName: '[name]__[local]__[hash:base64:5]',
+                                auto: /\.module\.\w+$/i,
+                            },
+                        },
+                    },
+                    'sass-loader',
+                ]
+            },
+            {
+                test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+                use: ['file-loader'],
+            },
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'public/index.html')
+        })
+    ],
+    devServer: {
+        port: 3000,
+        open: true,
+        hot: true,
+        historyApiFallback: true,
+      },
+    devtool: 'source-map'
+}
+
+//npx webpack --config webpack.config.js
